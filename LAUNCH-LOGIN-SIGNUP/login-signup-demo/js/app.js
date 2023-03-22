d61908e0185618fc0124058b12b253877d8952e8
Jesse Dong <dong@Jesses-MacBook-Air.local>
2023-03-22 14:35:24 -0700

    logic done, fixing syntax

--- LAUNCH-LOGIN-SIGNUP/login-signup-demo/js/app.js
+++ LAUNCH-LOGIN-SIGNUP/login-signup-demo/js/app.js
@@ -1,9 +1,5 @@
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-// get the database
-const db = firebase.firestore(); 
-// get the 3 collections
-const drivers = db.collection('drivers'); 
 
 function login() {
   const email = document.getElementById('email').value;
@@ -28,32 +24,53 @@ function login() {
 function signup() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(userCredential => {
-        // User signed up successfully
-        console.log(userCredential);
-        // Add user to DB 
-        addToDB(); 
-        // Redirect to login page or do other actions
-        window.location.href = 'index.html';
-      })
-      .catch(error => {
-        // Handle errors here
+
+    // Create user 
+    var creation = firebase.auth().createUserWithEmailAndPassword(email, password); 
+
+    // Check for errors 
+    creation.catch(function (error) {
+        var errorCode = error.code;
         document.getElementById("errormsg").innerHTML = error.message;
+    });
+
+    // Continue if no errors
+    creation.then(function () {
+        // var userUid = auth.currentUser.uid;
+        addToDB();
     });
 }
 
 function addToDB() {
-  // Add user to correct database collection
-  var userId = firebase.auth().currentUser.uid;
-  drivers.add({
-    first_name: "test",
-    last_name: "test last", 
-    address: "tesst addy"
-  }).then((docRef) => {
-    console.log("Success");
-  }).catch((error) => {
-    console.error("error");
-  });
+  // get the database
+  const db = firebase.firestore(); 
+  // Get current user 
+  var userUid = firebase.auth().currentUser.uid;
+  // add to the correct collection 
+  var fileName = location.pathname.split("/").slice(-1)
+  var col_name = " ";
+  if (fileName === "cust_login.html"){
+    col_name = "customers";
+  }
+  else if(fileName === "rest_login.html"){
+    col_name = "restaraunts";
+  }
+  else{
+    col_name = "drivers";
+  }
+  var col = db.collection(col_name);
+  console.log(col_name); 
+
+  // col.doc(userUid).set({
+  //   first_name: "test",
+  //   last_name: "test last", 
+  //   address: "tesst addy"
+  // }).then((docRef) => {
+  //   console.log("Success");
+  //   // Redirect to main page 
+  //   window.location.href = "index.html"; 
+  // }).catch((error) => {
+  //   console.error(error);
+  // });
 }
 
