4d056f809d864cc7699f4ae7777c5cc317ad349b
Jesse2131 <6jessed6@gmail.com>
2023-04-07 12:29:55 -0700

    reformatted

--- LAUNCH-LOGIN-SIGNUP/login-signup-demo/js/config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCnK0wvRERHCX991OnlnjiQO1FBD5B0szk",
-    authDomain: "odfds-4bee9.firebaseapp.com",
-    projectId: "odfds-4bee9",
-    storageBucket: "odfds-4bee9.appspot.com",
-    messagingSenderId: "659148068949",
-    appId: "1:659148068949:web:e9bac363e7afdc758b1cdc",
-    measurementId: "G-90TQ3NJ5BH"
-};
