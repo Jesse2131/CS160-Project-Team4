dd31de3e2b21dfc802f9f33f3ca63e40ef1cd9ce
Jesse Dong <dong@Jesses-MacBook-Air.local>
2023-04-19 15:42:46 -0700

    new files

---
+++ JS/add-menu.js
@@ -0,0 +1,12 @@
+function addToMenu(){
+    /*  Collection represents a restaurants menu(named Name-Menu, ie FoodInc-Menu)
+            Documents inside of collection represents each menu item
+                Name:
+                Price:
+                Image: 
+    */
+
+    // Get currently logged in rest user 
+    
+    
+}
