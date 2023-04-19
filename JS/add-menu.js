13d07b4cab373209b486360f1448db88bcab2c3f
Jesse2131 <6jessed6@gmail.com>
2023-04-19 16:10:44 -0700

    small changes

--- JS/add-menu.js
+++ JS/add-menu.js
@@ -1,12 +1,18 @@
-function addToMenu(){
+function addToMenu(event){
     /*  Collection represents a restaurants menu(named Name-Menu, ie FoodInc-Menu)
             Documents inside of collection represents each menu item
                 Name:
                 Price:
                 Image: 
     */
-
+    event.preventDefault();
     // Get currently logged in rest user 
+    const curr_user = firebase.auth().currentUser;
     
+    // Input form values
+    const item_name = document.getElementById("new-menu-item-name").value;
+    const item_price = document.getElementById("new-menu-item-price").value;
     
+    // Add document to collection and grab values from input forms 
+
 }
