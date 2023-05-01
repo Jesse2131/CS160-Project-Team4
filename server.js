e8b20ab55ed6dd84f11d5a892b8bd9a66ed50f6b
Jesse Dong <dong@Jesses-MacBook-Air.local>
2023-04-30 19:30:41 -0700

    reformatting for node js

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 13000;
+// For local, set host to localhost, for docker set to 0.0.0.0
+const HOST = 'localhost';
+
+// App
+const app = express();
+
+// When the app is first run, we go the the default page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/HTML/index.html');
+});
+
+// For all other pages load them from directories directly
+app.use(express.static(__dirname + '/HTML'));
+app.use(express.static(__dirname + '/public'));
+
+  
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
